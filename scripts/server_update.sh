a2d8886e6bcb0faf04773859a86a1a4f0fa315fa
nicholas.zhao <nicholas.zhao@bytedance.com>
2025-01-15 12:51:20 -0800

    fix

--- scripts/server_update.sh
+++ scripts/server_update.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 while true; do
+  # Kill process listening on port 3000
+  lsof -t -i tcp:3000 | xargs kill -9
+
+  # Kill process listening on port 3001
+  lsof -t -i tcp:3001 | xargs kill -9
+
   # Step 1: Run git pull
   git pull
 
   # Step 2: Run pnpm build
   cd app
+  pnpm install
   pnpm build
   # Step 3: Run pnpm start in a separate process
   pnpm start &
